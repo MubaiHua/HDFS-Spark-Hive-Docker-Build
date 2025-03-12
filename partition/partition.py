a9a1e507e5070776aff73f4f92eeb0ad730bc08b
Mubai Hua <mubaihua515@g.ucla.edu>
2025-03-11 18:25:45 -0700

    partition

---
+++ partition/partition.py
@@ -0,0 +1,21 @@
+import findspark
+findspark.init('/opt/spark')
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder \
+    .appName("PartitionedHiveTable") \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+# Read raw Parquet data
+df = spark.read.parquet("hdfs://master:8020/data/nyc_test/")
+df = df.withColumn("lpep_pickup_datetime", col("lpep_pickup_datetime").cast("timestamp"))
+df = df.withColumn("lpep_dropoff_datetime", col("lpep_dropoff_datetime").cast("timestamp"))
+
+# Write the data partitioned by the desired columns
+df.write.partitionBy("RatecodeID", "VendorID", "payment_type") \
+    .option("path", "hdfs://master:8020/data/nyc_taxi_partitioned") \
+    .mode("overwrite") \
+    .saveAsTable("taxi_data_partitioned")
